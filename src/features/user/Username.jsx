fbe49370c67eb50472aba7bd2732e263a89ac03f
Ha-Mundo <trauzzu@gmail.com>
2024-06-16 21:05:13 +0100

    added basic responsive design

--- src/features/user/Username.jsx
+++ src/features/user/Username.jsx
@@ -1,9 +1,5 @@
 function Username() {
-    return (
-        <div className="hidden text-sm font-semibold">
-            Hamundo
-        </div>
-    )
+  return <div className="hidden text-sm font-semibold">Hamundo</div>;
 }
 
-export default Username
+export default Username;
