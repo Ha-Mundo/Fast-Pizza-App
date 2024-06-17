144d8889befa5c1df9eace4674062415dfc08a81
Ha-Mundo <trauzzu@gmail.com>
2024-06-17 10:47:29 +0100

    added flex box rules

--- src/features/user/Username.jsx
+++ src/features/user/Username.jsx
@@ -1,5 +1,5 @@
 function Username() {
-  return <div className="hidden text-sm font-semibold">Hamundo</div>;
+  return <div className="hidden text-sm font-semibold md:block">Hamundo</div>;
 }
 
 export default Username;
