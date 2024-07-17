2c743a8119e67cedb11c86427134ebed8d55f721
Ha-Mundo <trauzzu@gmail.com>
2024-07-17 17:34:01 +0100

    small import bug typo fixed

--- src/features/cart/CartItem.jsx
+++ src/features/cart/CartItem.jsx
@@ -1,7 +1,7 @@
 import { useSelector } from 'react-redux';
 import { formatCurrency } from '../../utils/helpers';
 import UpdateItemQuantity from './UpdateItemQuantity';
-import DeleteItem from './deleteItem';
+import DeleteItem from './DeleteItem';
 import { getCurrentQuantityById } from './cartSlice';
 
 function CartItem({ item }) {
